5ccb102220cb571c38c3add5403e65c4370c2e49
A4KO <fynjif2002@yandex.ru>
2023-06-10 18:08:42 +1000

    1212

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+from pymem import *
+from pymem.process import *
 import PySimpleGUI as sg
-from PyQt6.QtGui import QPalette, QColor
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QVBoxLayout, QApplication, QWidget, QPushButton, QMainWindow, QLineEdit, QHBoxLayout, QGridLayout
-from PyQt6.QtGui import QIntValidator
 from ReadWriteMemory import ReadWriteMemory
 import win32process
 import win32api
@@ -11,12 +9,11 @@ from threading import Thread
 from pynput import keyboard
 from time import sleep
 import winsound
-import sys
 
 #variables
-points = {}
-check = {"coins": 0, 'bombs': 0, 'keys': 0, 'red_hearts': 0, "charges": 0, 'dmgs': 0, "blue_hearts": 0, "active_item": 0}
+address = 0
 coins_point=0
+base_address=0
 language = "ru"
 dmg =0
 red_heart=0
@@ -24,11 +21,11 @@ coins = 0
 bombs = 0
 keys = 0
 win_close=0
+process_hook_status=0
 charges=0
 blue_heart=0
 process_hook_stop=0
 ###############################################################################
-
 
 ###############################################################################
 
@@ -50,7 +47,7 @@ def layout(language):
         [sg.Text("")],
         [sg.Button('Выход',key="Exit"),sg.Text("",s=35),sg.Text("Made by GAVKOSHMIG Inc.")]]
     else:
-        main_window_layout = [  [sg.Text("Only integer numbers",s=47),sg.Button("Hook",key="new_run",s=6),sg.Button("RU/EN",key = "Change_language",s=6)],
+        main_window_layout = [  [sg.Text("Only integer numbers",s=49),sg.Button("Hook",key="new_run",s=6),sg.Button("RU/EN",key = "Change_language",s=5)],
         [sg.Text('Coins',s=8), sg.InputText(),sg.Button("Change",key="Change_coins"),sg.Button("Inf 99",key="inf_coins",s=4)],
         [sg.Text('Bombs',s=8), sg.InputText(),sg.Button("Change",key="Change_bombs"),sg.Button("Inf 99",key="inf_bombs",s=4)],
         [sg.Text('Keys',s=8), sg.InputText(),sg.Button("Change",key="Change_keys"),sg.Button("Inf 99",key="inf_keys",s=4)],
@@ -66,282 +63,312 @@ def layout(language):
     return sg.Window("Isaac's cheats",main_window_layout)
 ###############################################################################
 
-
+#error window cfg
+error_window_layout = [[sg.Text("Your input not integer number!")],
+           [sg.Button("Close",key="Close_error")]]
+error_window = sg.Window("Error",error_window_layout)
 ###############################################################################
 
-def writei(name, count):
-    process.write(points[name],count)
+#write funcs
+def coins_write(coins_count):
+    process.write(coins_point,coins_count)
+def bombs_write(bombs_count):
+    process.write(bombs_point,bombs_count)
+def keys_write(keys_count):
+    process.write(keys_point,keys_count)
+def damage_write():
+    process.write(damage_point,1120403000)
+def red_heart_write():
+    process.write(red_heart_point, 24)
+def charges_write():
+    process.write(charges_point, 6)
+def active_item_write(item_id):
+    process.write(active_item_point,item_id)
+def blue_heart_write():
+    process.write(blue_heart_point, 8)
 
-def languageChange():
-    window.close()
-    if language == "ru":
-        language = "en"
-        window = (layout(language))
-    else:
-        language = "ru"
-        window = (layout(language))
 
-def close():
-    global win_close
-    win_close=1
-    #window.close()    
 
 ###############################################################################
 
 #error window
 def write_and_error_window(value,thing):
     try:
-        writei(thing, int(value))
+        value = int(value)
+        if thing == "coins":
+            coins_write(int(value))
+        elif thing == "bombs":
+            bombs_write(int(value))
+        elif thing == "keys":
+            keys_write(int(value))
+        elif thing == "active_item":
+            active_item_write(int(value))
     except:
-        winsound.PlaySound("ButtonClick.wav", 1)
-        text = "ERROR!"
-        if language == "ru": 
-            text = "ERROR!"
-        sg.popup(text, title="ERROR!")
-
-###############################################################################
-
-class Changeable:
-    def __init__(self, name, num = 99, line = False, checked = True) -> None:
-        self.name = name
-        self.num = num
-        if line:
-            window.create_line(name, checked)
-        else:
-            window.create_button(name, checked)
-    def infWrite(self):
-        global check
-        if check[self.name] == 0:
-            check[self.name] = 1
-            def everlasting():
-                global check
-                while check[self.name] == 1:
-                        writei(self.name, self.num)
-                        sleep(1)
-            Thread(target=everlasting).start()
-        elif check[self.name] == 1:
-            check[self.name] = 0
-    def write(self, value):
-        try:
-            writei(self.name, int(value))
-        except:
+        if language == "ru":
             winsound.PlaySound("ButtonClick.wav", 1)
-            text = "Your input isn't integer number!"
-            if language == "ru": 
-                text = "Вы ввели не целое число!"
-            sg.popup(text, title="ERROR!")
-
-
-
-###############################################################################
-#-----НОВОЕ ОКНО------
-
-#Кнопка ого
-class Button(QPushButton):
-    def __init__(self, name, checked = True):
-        super(Button, self).__init__()
-        self.name = name
-        self.setText(name)
-        if checked:
-            self.setCheckable(True)
-        self.clicked.connect(self.the_button_was_toggled)
-        if checked:
-            try:
-                self.setChecked(check[self.name])
-            except:
-                pass
-
-    def the_button_was_toggled(self, checked):
-        check[self.name] = checked
-        print(self.name, check[self.name])
-
-class SpecialButton(QPushButton):
-    def __init__(self, name):
-        super(QPushButton, self).__init__()
-        self.setText(name)
-        self.setBaseSize(10, 10)
-        if name == "Hook":
-            self.clicked.connect(self.hook)
+            sg.popup("Вы ввели не целое число!",title="ERROR!")
         else:
-            self.clicked.connect(self.lang)
-        
-    def hook(self):
-        pass
+            winsound.PlaySound("ButtonClick.wav", 1)
+            sg.popup("Your input isn't integer number!",title="ERROR!")
+###############################################################################
 
-    def lang(self):
-        pass
+#loop new
+def inf_coins():
+    global coins
+    if coins == 0:
+        coins = 1
+        def everlasting_coins():
+            global coins
+            while coins == 1:
+                    coins_write(99)
+                    sleep(1)
+        Thread(target=everlasting_coins).start()
+    elif coins == 1:
+        coins = 0
 
-class LineEdit(QHBoxLayout):
-    def __init__(self, name, checked=True):
-        super(QHBoxLayout, self).__init__()
-        self.name = name
-        self.input = QLineEdit()
-        self.onlyInt = QIntValidator()
-        self.onlyInt.setRange(0, 999)
-        self.input.setValidator(self.onlyInt)
-        self.input.setFixedWidth(150)
-        self.addWidget(self.input)
-        self.button1 = QPushButton("Изменить")
-        self.button2 = Button(name, checked)
-        self.button1.clicked.connect(self.get)
-        self.addWidget(self.button1)
-        self.addWidget(self.button2)
+def inf_bombs():
+    global bombs
+    if bombs == 0:
+        bombs = 1
+        def everlasting_bombs():
+            global bombs
+            while bombs == 1:
+                bombs_write(99)
+                sleep(1)
+        Thread(target=everlasting_bombs).start()
+    elif bombs == 1:
+        bombs = 0
 
-    def get(self):
-        text = self.input.text()
-        print(text)
+def inf_keys():
+    global keys
+    if keys == 0:
+        keys = 1
+        def everlasting_keys():
+            global keys
+            while keys == 1:
+                keys_write(99)
+                sleep(1)
+        Thread(target=everlasting_keys).start()
+    elif keys == 1:
+        keys = 0
 
-#Класс окна, все настраиваем тут
-class MainWindow(QMainWindow):
+def inf_red_heart():
+    global red_heart
+    if red_heart == 0:
+        red_heart = 1
+        def everlasting_red_heart():
+            global red_heart
+            while red_heart == 1:
+                red_heart_write()
+                sleep(1)
+        Thread(target=everlasting_red_heart).start()
+    elif red_heart == 1:
+        red_heart = 0
 
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Isaac Cheats")
-        self.setFixedSize(330, 290)
-        self.layout = QVBoxLayout()
-        self.button_checked = {}
+def inf_charges():
+    global charges
+    if charges == 0:
+        charges = 1
+        def everlasting_charges():
+            global charges
+            while charges == 1:
+                charges_write()
+                sleep(0.2)
+        Thread(target=everlasting_charges).start()
+    elif charges == 1:
+        charges = 0
 
-    def create_button(self, name, checked = True):
-        try:
-            self.button_checked[name] = False
-        except:
-            pass
-        button = Button(name, checked)
-        self.layout.addWidget(button)
+def inf_100_dmg():
+    global dmg
+    if dmg == 0:
+        dmg = 1
+        def everlasting_dmg():
+            global dmg
+            while dmg == 1:
+                damage_write()
+                sleep(1)
+        Thread(target=everlasting_dmg).start()
+    elif dmg == 1:
+        dmg = 0
 
-    def create_line(self, name, checked):
-        self.button_checked[name] = False
-        line = LineEdit(name, checked)
-        self.layout.addLayout(line)
-
-    def initiate(self):
-        self.widget = QWidget()
-        self.widget.setLayout(self.layout)
-        self.setCentralWidget(self.widget)
-
-    def addLayout(self, layout):
-        self.layout.addLayout(layout)
-
-app = QApplication(sys.argv)
-window = MainWindow()
-
-fbuttons = QGridLayout()
-hook = SpecialButton("Hook")
-lang = SpecialButton("EN/RU")
-fbuttons.addWidget(hook)
-fbuttons.addWidget(lang)
-window.addLayout(fbuttons)
-
-coin = Changeable("coins", line=True)
-bomb = Changeable("bombs", line=True)
-key = Changeable("keys", line=True)
-active = Changeable("active_item", line=True, checked=False)
-red_hearts = Changeable("red_hearts", 24)
-charge = Changeable("charges", 6)
-blue_hearts = Changeable("blue_hearts", 8)
-dmgs = Changeable("dmgs", 1120403000)
-
-window.initiate()
-window.show()
-app.exec() #Открытие
+def inf_blue_heart():
+    global blue_heart
+    if blue_heart == 0:
+        blue_heart = 1
+        def everlasting_blue_heart():
+            global blue_heart
+            while blue_heart == 1:
+                blue_heart_write()
+                sleep(1)
+        Thread(target=everlasting_blue_heart).start()
+    elif blue_heart == 1:
+        blue_heart = 0
 
 ###############################################################################
 
 #window logic
-#window = (layout(language))
+window = (layout(language))
 def main():
-    global coins_point,address,process,process_hook_status,win_close
+    global coins_point,bombs_point,keys_point,damage_point,address,process,process_hook_status,red_heart_point,charges_point,active_item_point,blue_heart_point,win_close
     global process,base_address
     global coins, bombs,keys,win_close,process_hook_status,language,window
-    #event, values = window.read(timeout=10)
+    event, values = window.read(timeout=10)
+    print(event)
+    #loop things
+    if event =="inf_coins":
+        inf_coins()
+    if event == "inf_bombs":
+        inf_bombs()
+    if event == "inf_keys":
+        inf_keys()
+    if event == "inf_red_heart":
+        inf_red_heart()
+    if event == "inf_charges":
+        inf_charges()
+    if event == "100 Damage":
+        inf_100_dmg()
+    if event == "inf_blue_heart":
+        inf_blue_heart()
 
-    #if values == None:
-    values = [None, None, None, None]
-    event = None
 
-    eventFunc = {
-        "inf_coins": coin.infWrite,
-        "inf_bombs": bomb.infWrite,
-        "inf_keys": key.infWrite,
-        "inf_red_heart": red_hearts.infWrite,
-        "inf_charges": charge.infWrite,
-        "100 Damage": dmgs.infWrite,
-        "inf_blue_heart": blue_hearts.infWrite,
-        "Change_coins": [coin.write, values[0]],
-        "Change_bombs": [bomb.write, values[1]],
-        "Change_keys": [key.write, values[2]],
-        "Change_active_item": [active.write, values[3]],
-        "give_d6": [active.write, 105],
-        "new_run":  inf_hook,
-        "Change_language": languageChange,
-        "Exit": close,
-        sg.WIN_CLOSED: close
-    }
-
-    if event == '__TIMEOUT__':
-        pass
-    elif type(eventFunc[event]) == list:
-        eventFunc[event][0](eventFunc[event][1])
-    else:
-        eventFunc[event]()
-  
+    #writtable things
+    if event == "Change_coins":
+        write_and_error_window(values[0],"coins")
+    if event == "Change_bombs":
+        write_and_error_window(values[1],"bombs")
+    if event == "Change_keys":
+        write_and_error_window(values[2],"keys")
+    if event == "Change_active_item":
+        write_and_error_window(values[3],"active_item")
+    
+    #misc
+    if event == "new_run":
+        try:
+            inf_hook()
+        except: pass
+    if event == "give_d6":
+        write_and_error_window(105,"active_item")
+    if event == "Change_language":
+        window.close()
+        if language == "ru":
+            language = "en"
+            window = (layout(language))
+        else:
+            language = "ru"
+            window = (layout(language))
+    #exit
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        win_close=1
+        window.close()    
 ###############################################################################
 
 #hotkeys
 def hotkeys():
     global hotkey
     with keyboard.GlobalHotKeys({
-        "<ctrl>+1":coin.infWrite,
-        "<ctrl>+2":bomb.infWrite,
-        "<ctrl>+3":key.infWrite,
-        "<ctrl>+4":dmgs.infWrite,
-        "<ctrl>+5":red_hearts.infWrite,
-        "<ctrl>+6":blue_hearts.infWrite,
-        "<ctrl>+7":charge.infWrite}) as hotkey:
+        "<ctrl>+1":inf_coins,
+        "<ctrl>+2":inf_bombs,
+        "<ctrl>+3":inf_keys,
+        "<ctrl>+4":inf_100_dmg,
+        "<ctrl>+5":inf_red_heart,
+        "<ctrl>+6":inf_blue_heart,
+        "<ctrl>+7":inf_charges}) as hotkey:
         hotkey.join()
 Thread(target=hotkeys).start()
+###############################################################################
+
+
 
 ###############################################################################
 
+
+
+
 #process hook and pointers
 def inf_hook():
-    try:
-        global coins_point,process,points
-        process = ReadWriteMemory().get_process_by_name("isaac-ng.exe")
-        process.open()
-        address = process.get_base_address()+0x804270        
-        points["coins"] = process.get_pointer(address, offsets=[0x4,0x1C,0x9D0,0x358,0x0,0x12B8])
-        points["bombs"] = process.get_pointer(address, offsets=[0x4,0x1C,0x9D0,0x23C,0x16C,0x0,0x12B4])
-        points["keys"] = process.get_pointer(address, offsets=[0x4,0x1C,0xBBC,0x16C,0x0,0x12AC])
-        points["dmgs"] = process.get_pointer(address,offsets=[0x4,0x1C,0x9D0,0x23C,0x16C,0x0,0x13B8])
-        points["red_hearts"] = process.get_pointer(address,offsets=[0x4,0x1C,0x9F0,0x23C,0x16C,0x0,0x1294])
-        points["charges"] = process.get_pointer(address,offsets=[0x4,0x0,0x4,0x1C,0xCF8,0x0,0x14C8])
-        points["active_item"] = process.get_pointer(address,offsets=[0x8,0x1C,0x9F0,0x30,0x358,0x0,0x14C4])
-        points["blue_hearts"] = process.get_pointer(address,offsets=[0x4,0x1C,0x9D0,0x50,0x358,0x0,0x129C])
+    global coins_point,bombs_point,keys_point,damage_point,address,process,process_hook_status,red_heart_point,charges_point,active_item_point,blue_heart_point,win_close
+    global charges,red_heart,blue_heart,coins,bombs,keys,base_address
+    my_pid = None
+    pids = None
+    ps = None
+    pids = psutil.pids()
+    for pid in pids:
+        ps = psutil.Process(pid) 
+        if "isaac-ng.exe" in ps.name():
+            my_pid = ps.pid
+    PROCESS_ALL_ACCESS = 0x1F0FFF
+    processHandle = win32api.OpenProcess(PROCESS_ALL_ACCESS, False, my_pid)
+    modules = win32process.EnumProcessModules(processHandle)
+    processHandle.close()
+    base_address = modules[0]
+    print(base_address)
+    rwm = ReadWriteMemory()
+    process = rwm.get_process_by_name("isaac-ng.exe")
+    process.open()
+    pm = Pymem("isaac-ng.exe")
+    base_address = pm.base_address
+    address = base_address+0x804270
 
-        if coins_point == 4792:
-            if language == "ru":
-                winsound.PlaySound("ButtonClick.wav", 1)
-                sg.popup("Невозможно захватить процесс, перезапустите игру!",title="ERROR!")
-            else:
-                winsound.PlaySound("ButtonClick.wav", 1)
-                sg.popup("Can't hook process, restart the game!",title="ERROR!")
-            for i in check:
-                i = 0
-    except:
-        pass
+
+    coins_point = process.get_pointer(address, offsets=[0x4,0x1C,0x9D0,0x358,0x0,0x12B8])
+    process_hook_status = 1
+    bombs_point = process.get_pointer(address, offsets=[0x4,0x1C,0x9D0,0x23C,0x16C,0x0,0x12B4])
+    keys_point = process.get_pointer(address, offsets=[0x4,0x1C,0xBBC,0x16C,0x0,0x12AC])
+    damage_point = process.get_pointer(address,offsets=[0x4,0x1C,0x9D0,0x23C,0x16C,0x0,0x13B8])
+    red_heart_point = process.get_pointer(address,offsets=[0x4,0x1C,0x9F0,0x23C,0x16C,0x0,0x1294])
+    charges_point = process.get_pointer(address,offsets=[0x4,0x0,0x4,0x1C,0xCF8,0x0,0x14C8])
+    active_item_point = process.get_pointer(address,offsets=[0x8,0x1C,0x9F0,0x30,0x358,0x0,0x14C4])
+    blue_heart_point = process.get_pointer(address,offsets=[0x4,0x1C,0x9D0,0x50,0x358,0x0,0x129C])
+
+
+    if coins_point == 4792:
+        if language == "ru":
+            winsound.PlaySound("ButtonClick.wav", 1)
+            sg.popup("Невозможно захватить процесс, перезапустите игру!",title="ERROR!")
+        else:
+            winsound.PlaySound("ButtonClick.wav", 1)
+            sg.popup("Can't hook process, restart the game!",title="ERROR!")
+        charges = 0
+        red_heart=0
+        coins = 0
+        bombs = 0
+        keys = 0
+        blue_heart=0
+
+
+    pm = Pymem("isaac-ng.exe")    
+    base_pymem = pm.base_address+0x804270
+
+
+    def GetPtrAddr(base,offsets):
+        print("govno")
+        addr = pm.read_longlong(base)
+        for i in offsets:
+            print(i,"    ", offsets[-1])
+            if i != offsets[-1]:
+                addr = pm.read_longlong(addr+i)
+                print(addr)
+        print(addr+offsets[-1])
+        return addr+offsets[-1]
+
+    print(pm.read_int(GetPtrAddr(base_pymem, [0x4,0x1C,0x9D0,0x358,0x0,0x12B8])),"vyuio")
+
 
 
 ###############################################################################
 
 #baza 
 main()
-# while True:
-#     if win_close==1:
-#         for i in check:
-#             i = 0
-#         #hotkey.stop()
-#         break
-#     else:
-#         main()
+while True:
+    if win_close==1:
+        charges = 0
+        red_heart=0
+        coins = 0
+        bombs = 0
+        keys = 0
+        blue_heart=0
+        hotkey.stop()
+        break
+    else:
+        main()
 ###############################################################################
